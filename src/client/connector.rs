79b12887585f321f16ed2f76e5c4a610431cb341
Lorenzo Leonardo <enzotechcomputersolutions@gmail.com>
2023-10-30 07:59:35 +0800

    create a client library

---
+++ src/client/connector.rs
@@ -0,0 +1,87 @@
+use std::collections::HashMap;
+
+use serde_derive::{Deserialize, Serialize};
+
+#[derive(Serialize, Deserialize, Debug)]
+pub struct RegisterObject {
+    reg_object: String,
+}
+
+impl RegisterObject {
+    pub fn new(name: &str) -> Self {
+        Self {
+            reg_object: name.to_string(),
+        }
+    }
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+pub struct Success {
+    success: String,
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+pub struct Error {
+    error: String,
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+pub struct CallObjectRequest {
+    pub object: String,
+    pub method: String,
+    #[serde(skip_serializing_if = "Option::is_none")]
+    pub param: Option<HashMap<String, String>>,
+}
+
+impl CallObjectRequest {
+    pub fn new(object: &str, method: &str) -> Self {
+        Self {
+            object: object.to_string(),
+            method: method.to_string(),
+            param: None,
+        }
+    }
+
+    pub fn parameter(mut self, key: &str, value: &str) -> Self {
+        if self.param.is_none() {
+            self.param = Some(HashMap::new());
+        }
+
+        if let Some(param) = &mut self.param {
+            param.insert(key.to_owned(), value.to_owned());
+        }
+
+        self
+    }
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+pub struct CallObjectResponse {
+    pub response: String,
+}
+
+impl CallObjectResponse {
+    pub fn new(response: &str) -> Self {
+        Self {
+            response: response.to_string(),
+        }
+    }
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+#[serde(untagged)]
+pub enum IncomingMessage {
+    Register(Success),
+    Error(Error),
+    CallRequest(CallObjectRequest),
+    CallResponse(CallObjectResponse),
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+#[serde(untagged)]
+pub enum OutgoingMessage {
+    Register(RegisterObject),
+    Error(Error),
+    CallRequest(CallObjectRequest),
+    CallResponse(CallObjectResponse),
+}
--- src/test/server.rs
+++ src/test/server.rs
@@ -1,96 +1,14 @@
 use std::collections::HashMap;
 
-use serde_derive::{Deserialize, Serialize};
+use ipc_client::client::connector::{
+    CallObjectRequest, CallObjectResponse, IncomingMessage, OutgoingMessage, RegisterObject,
+};
 use tokio::io::{AsyncReadExt, AsyncWriteExt};
 use tokio::net::TcpStream;
 use tokio::sync::mpsc::unbounded_channel;
 
 use crate::manager::TaskManager;
 use crate::Server;
-
-#[derive(Serialize, Deserialize, Debug)]
-struct RegisterObject {
-    reg_object: String,
-}
-
-impl RegisterObject {
-    fn new(name: &str) -> Self {
-        Self {
-            reg_object: name.to_string(),
-        }
-    }
-}
-
-#[derive(Serialize, Deserialize, Debug)]
-struct Success {
-    success: String,
-}
-
-#[derive(Serialize, Deserialize, Debug)]
-struct Error {
-    error: String,
-}
-
-#[derive(Serialize, Deserialize, Debug)]
-pub struct CallObjectRequest {
-    pub object: String,
-    pub method: String,
-    #[serde(skip_serializing_if = "Option::is_none")]
-    pub param: Option<HashMap<String, String>>,
-}
-
-impl CallObjectRequest {
-    pub fn new(object: &str, method: &str) -> Self {
-        Self {
-            object: object.to_string(),
-            method: method.to_string(),
-            param: None,
-        }
-    }
-
-    pub fn parameter(mut self, key: &str, value: &str) -> Self {
-        if self.param.is_none() {
-            self.param = Some(HashMap::new());
-        }
-
-        if let Some(param) = &mut self.param {
-            param.insert(key.to_owned(), value.to_owned());
-        }
-
-        self
-    }
-}
-
-#[derive(Serialize, Deserialize, Debug)]
-pub struct CallObjectResponse {
-    pub response: String,
-}
-
-impl CallObjectResponse {
-    pub fn new(response: &str) -> Self {
-        Self {
-            response: response.to_string(),
-        }
-    }
-}
-
-#[derive(Serialize, Deserialize, Debug)]
-#[serde(untagged)]
-enum IncomingMessage {
-    Register(Success),
-    Error(Error),
-    CallRequest(CallObjectRequest),
-    CallResponse(CallObjectResponse),
-}
-
-#[derive(Serialize, Deserialize, Debug)]
-#[serde(untagged)]
-enum OutgoingMessage {
-    Register(RegisterObject),
-    Error(Error),
-    CallRequest(CallObjectRequest),
-    CallResponse(CallObjectResponse),
-}
 
 #[tokio::test]
 async fn test_server() {
