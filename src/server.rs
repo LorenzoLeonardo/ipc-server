a92b3854ac38abb374ef962e8452c184e9d4ee4d
Lorenzo Leonardo <enzotechcomputersolutions@gmail.com>
2023-10-31 15:58:30 +0800

    improve reading of data from stream
    The read buffer must be dynamic, we will never know
    how much is the data being sent to us.

--- src/server.rs
+++ src/server.rs
@@ -1,5 +1,6 @@
 use std::sync::Arc;
 
+use tokio::io::Interest;
 use tokio::sync::Mutex;
 use tokio::{
     io::AsyncWriteExt,
@@ -30,50 +31,57 @@ impl Server {
         let ip = socket.peer_addr().unwrap().to_string();
         log::trace!("[{}]: Client connected", ip);
 
-        let mut buffer = [0u8; u16::MAX as usize];
         let tcp = Arc::new(Mutex::new(socket));
 
         loop {
+            let mut buffer = Vec::new();
             // Use try_read to check if there is data available to read without blocking.
             let mut socket = tcp.lock().await;
-            if let Ok(bytes_read) = socket.try_read(&mut buffer) {
-                if bytes_read == 0 {
-                    // The client has closed the connection.
-                    break;
-                }
-                let received_data = &buffer[..bytes_read];
-                match serde_json::from_slice(received_data) {
-                    Ok(ipc_message) => {
-                        let session: Session = Session::new(ipc_message, ip.clone(), tcp.clone());
+            let ready = socket
+                .ready(Interest::READABLE | Interest::WRITABLE)
+                .await
+                .unwrap();
 
-                        let (oneshot_tx, oneshot_rx) = oneshot::channel();
-                        tx.send(Message::ProcessInput(session, oneshot_tx))
-                            .unwrap_or_else(|e| {
+            if ready.is_readable() {
+                if let Ok(bytes_read) = socket.try_read_buf(&mut buffer) {
+                    if bytes_read == 0 {
+                        // The client has closed the connection.
+                        break;
+                    }
+                    match serde_json::from_slice(buffer.as_slice()) {
+                        Ok(ipc_message) => {
+                            let session: Session =
+                                Session::new(ipc_message, ip.clone(), tcp.clone());
+
+                            let (oneshot_tx, oneshot_rx) = oneshot::channel();
+                            tx.send(Message::ProcessInput(session, oneshot_tx))
+                                .unwrap_or_else(|e| {
+                                    log::error!("{:?}", e);
+                                });
+
+                            let reply = oneshot_rx.await.unwrap_or_else(|e| {
                                 log::error!("{:?}", e);
+                                Vec::new()
                             });
-
-                        let reply = oneshot_rx.await.unwrap_or_else(|e| {
-                            log::error!("{:?}", e);
-                            Vec::new()
-                        });
-                        if let Err(e) = socket.write_all(reply.as_slice()).await {
-                            log::error!("Error writing data to client: {}", e);
-                            break;
+                            if let Err(e) = socket.write_all(reply.as_slice()).await {
+                                log::error!("Error writing data to client: {}", e);
+                                break;
+                            }
+                        }
+                        Err(e) => {
+                            if let Err(e) = socket
+                                .write_all(&Error::new(e.to_string().as_str()).serialize().unwrap())
+                                .await
+                            {
+                                log::error!("Error writing data to client: {}", e);
+                                break;
+                            }
                         }
                     }
-                    Err(e) => {
-                        if let Err(e) = socket
-                            .write_all(&Error::new(e.to_string().as_str()).serialize().unwrap())
-                            .await
-                        {
-                            log::error!("Error writing data to client: {}", e);
-                            break;
-                        }
-                    }
+                } else {
+                    // No data available to read, continue other tasks or operations.
+                    tokio::task::yield_now().await;
                 }
-            } else {
-                // No data available to read, continue other tasks or operations.
-                tokio::task::yield_now().await;
             }
         }
 
