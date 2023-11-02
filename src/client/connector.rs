0509473204acb53c5d64a2b18ea6086c885115b5
Lorenzo Leonardo <enzotechcomputersolutions@gmail.com>
2023-11-02 14:35:08 +0800

    implement send_event and listen_for_event

--- src/client/connector.rs
+++ src/client/connector.rs
@@ -1,4 +1,5 @@
 use std::collections::HashMap;
+use std::future::Future;
 use std::sync::Arc;
 
 use tokio::io::{AsyncReadExt, AsyncWriteExt};
@@ -6,7 +7,7 @@ use tokio::net::TcpStream;
 use tokio::sync::Mutex;
 
 use super::error::Error;
-use super::message::{CallObjectRequest, JsonValue};
+use super::message::{CallObjectRequest, Event, JsonValue, SubscribeToEvent};
 
 use crate::SERVER_ADDRESS;
 
@@ -60,5 +61,77 @@ impl Connector {
         } else {
             Ok(buf[0..n].to_vec())
         }
+    }
+
+    /// Send the event to the ipc-server and let ipc-server boadcast the message
+    /// to all subscribed processes.
+    pub async fn send_event(&self, event: &str, result: JsonValue) -> Result<(), Error> {
+        let request = Event::new(event, result);
+
+        let mut socket = self.socket.lock().await;
+
+        socket
+            .write_all(
+                request
+                    .serialize()
+                    .map_err(|e| Error::Serde(e.to_string()))?
+                    .as_slice(),
+            )
+            .await
+            .map_err(|e| Error::Io(e.to_string()))?;
+        Ok(())
+    }
+
+    pub async fn listen_for_event<
+        F: Future<Output = Result<(), RE>> + Send,
+        RE: std::error::Error + 'static + Send,
+        T: Fn(JsonValue) -> F + Send + Sync + 'static,
+    >(
+        &self,
+        event_name: &str,
+        callback: T,
+    ) -> Result<(), Error> {
+        let request = SubscribeToEvent::new(event_name);
+
+        let mut socket = self.socket.lock().await;
+
+        socket
+            .write_all(
+                request
+                    .serialize()
+                    .map_err(|e| Error::Serde(e.to_string()))?
+                    .as_slice(),
+            )
+            .await
+            .map_err(|e| Error::Io(e.to_string()))?;
+
+        let socket = self.socket.clone();
+
+        tokio::spawn(async move {
+            let mut socket = socket.lock().await;
+
+            let mut buf = [0u8; u16::MAX as usize];
+            let n = socket
+                .read(&mut buf)
+                .await
+                .map_err(|e| Error::Io(e.to_string()))
+                .unwrap_or_else(|e| {
+                    log::error!("{:?}", e);
+                    0
+                });
+
+            let value: JsonValue = serde_json::from_slice(&buf[0..n])
+                .map_err(|e| Error::Serde(e.to_string()))
+                .unwrap_or_else(|e| {
+                    log::error!("{:?}", e);
+                    JsonValue::Bool(false)
+                });
+
+            log::trace!("{:?}", &value);
+            callback(value).await.unwrap_or_else(|e| {
+                log::error!("{:?}", e);
+            });
+        });
+        Ok(())
     }
 }
